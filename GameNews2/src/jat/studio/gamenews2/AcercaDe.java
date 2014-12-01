cd1fbc0006a4ba30b9d88622227a5118de2422bd
Toni_re <tonire.rebollo@gmail.com>
2014-12-02 00:23:51 +0100

    AcercaDe mejorado

--- GameNews2/src/jat/studio/gamenews2/AcercaDe.java
+++ GameNews2/src/jat/studio/gamenews2/AcercaDe.java
@@ -1,10 +1,15 @@
 package jat.studio.gamenews2;
 
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
 import android.support.v7.app.ActionBarActivity;
+import android.view.View;
+import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
 import android.widget.SimpleAdapter;
+import android.widget.Toast;
 
 import java.util.HashMap;
 import java.util.LinkedList;
@@ -18,7 +23,9 @@ public class AcercaDe extends ActionBarActivity {
 
     public final static String ITEM_TITLE = "title";
     public final static String ITEM_CAPTION = "caption";
-
+    private int contadorToni = 0;
+    private String url;
+    private Intent i;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -38,6 +45,50 @@ public class AcercaDe extends ActionBarActivity {
 
         ListView list = (ListView)findViewById(R.id.listView);
         list.setAdapter(adapter);
+        list.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                switch(position){
+                    case 1:
+                        contadorToni = contadorToni+1;
+                        if(contadorToni>=10){
+                            Toast.makeText(AcercaDe.this,"Oh no!\nHas encontrado mi secreto. Será mejor que no se lo cuentes a nadie... <3<3",Toast.LENGTH_LONG).show();
+                            contadorToni = 0;
+                        }
+                        break;
+                    case 5:
+                        //PanoramaGL
+                        url = "https://code.google.com/p/panoramagl-android/";
+                        i = new Intent(Intent.ACTION_VIEW);
+                        i.setData(Uri.parse(url));
+                        startActivity(i);
+                        break;
+                    case 6:
+                        //Picasso
+                        url = "http://square.github.io/picasso/";
+                        i = new Intent(Intent.ACTION_VIEW);
+                        i.setData(Uri.parse(url));
+                        startActivity(i);
+                        break;
+
+                    case 7:
+                        //Barcodafraglib v2
+                        url = "https://code.google.com/p/barcodefraglibv2/";
+                        i = new Intent(Intent.ACTION_VIEW);
+                        i.setData(Uri.parse(url));
+                        startActivity(i);
+                        break;
+
+                    case 8:
+                        //Page Curl
+                        url = "https://github.com/cags12/android_page_curl";
+                        i = new Intent(Intent.ACTION_VIEW);
+                        i.setData(Uri.parse(url));
+                        startActivity(i);
+                        break;
+                }
+            }
+        });
 
     }
     public Map<String,?> createItem(String title, String caption) {
