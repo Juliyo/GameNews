50e939c6049405fdb20ef9a51f599336459e9142
Toni_re <tonire.rebollo@gmail.com>
2014-12-01 20:08:41 +0100

    Noticias OP

---
+++ GameNews2/src/jat/studio/gamenews2/AcercaDe.java
@@ -0,0 +1,17 @@
+package jat.studio.gamenews2;
+
+import android.os.Bundle;
+import android.support.v7.app.ActionBarActivity;
+
+/**
+ * Created by Toni on 01/12/2014.
+ */
+public class AcercaDe extends ActionBarActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.acerca_activity);
+
+    }
+}
